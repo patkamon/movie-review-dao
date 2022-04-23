57d3b8f59cff447d4b6de961419ced8ca6777b44
patkamon <patkamon.a@ku.th>
2022-04-23 17:34:30 +0700

    fix ui for query movie

--- ui.py
+++ ui.py
@@ -4,6 +4,9 @@ from tkinter import ttk
 import models
 
 list =[]
+list = models.session.query(models.Movie)
+m = list[0]
+
 
 def click():
     entered_text= textEntry.get()
@@ -11,15 +14,20 @@ def click():
     for m in models.session.query(models.Movie):
         if m.title.find(entered_text) != -1:
             list.append(m.title)
-    # for index, m in enumerate(list):
-    #     ttk.Label(frm, text=m.title).grid(column=1, row=1+index)
-    #     ttk.Label(frm, text=m.genre).grid(column=2, row=1+index)
-    #     ttk.Label(frm, text=m.lead_studio).grid(column=3, row=1+index)
-    #     ttk.Label(frm, text=m.profit).grid(column=4, row=1+index)
-    #     ttk.Label(frm, text=m.ww_gross).grid(column=5, row=1+index)
-    #     ttk.Label(frm, text=m.year).grid(column=6, row=1+index)
-    #     if index >= 9:
-    #         break;
+
+    list = sorted(list,key=len)
+    try:
+        m  = models.session.query(models.Movie).filter_by(title=list[0]).first()
+        title.set(f'{m.title} |')
+        genre.set(f'{m.genre} |')
+        lead_studio.set(f'{m.lead_studio} |')
+        profit.set(f'{m.profit} |')
+        gross.set(f'{m.ww_gross} |')
+        year.set(m.year)
+
+
+    except:
+        pass
     output.delete(0.0,END)
     output.insert(END,", ".join(list))
 
@@ -39,25 +47,38 @@ ttk.Label(frm, text="Profit").grid(column=4, row=0)
 ttk.Label(frm, text="Gross").grid(column=5, row=0)
 ttk.Label(frm, text="Year").grid(column=6, row=0)
 
-list = models.session.query(models.Movie)
 
+title = StringVar()
+title.set(f'{m.title} |')
 
-for index, m in enumerate(list):
-    ttk.Label(frm, text=m.title).grid(column=1, row=1+index)
-    ttk.Label(frm, text=m.genre).grid(column=2, row=1+index)
-    ttk.Label(frm, text=m.lead_studio).grid(column=3, row=1+index)
-    ttk.Label(frm, text=m.profit).grid(column=4, row=1+index)
-    ttk.Label(frm, text=m.ww_gross).grid(column=5, row=1+index)
-    ttk.Label(frm, text=m.year).grid(column=6, row=1+index)
-    if index >= 9:
-        break;
+genre = StringVar()
+genre.set(f'{m.genre} |')
+
+lead_studio = StringVar()
+lead_studio.set(f'{m.lead_studio} |')
+
+profit = StringVar()
+profit.set(f'{m.profit} |')
+
+gross = StringVar()
+gross.set(f'{m.ww_gross} |')
+
+year = StringVar()
+year.set(m.year)
+
+ttk.Label(frm, textvariable=title).grid(column=1, row=1)
+ttk.Label(frm, textvariable=genre).grid(column=2, row=1)
+ttk.Label(frm, textvariable=lead_studio).grid(column=3, row=1)
+ttk.Label(frm, textvariable=profit).grid(column=4, row=1)
+ttk.Label(frm, textvariable=gross).grid(column=5, row=1)
+ttk.Label(frm, textvariable=year).grid(column=6, row=1)
 
 textEntry = Entry(window, width = 20)
 textEntry.grid(column=0,row= 20)
 
 ttk.Button(window, text='submit', width=6,command=click).grid(column=0,row=21)
 
-output= Text(window, width =75 ,height =6, wrap=WORD,background='white' )
+output= Text(window, width =75 ,height =6, wrap=WORD,background='cyan' )
 output.grid(column=0,row=22)
 
 
