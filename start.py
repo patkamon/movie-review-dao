eb3557165b93720efa15c088fe095e92799a6f96
patkamon <patkamon.a@ku.th>
2022-04-24 17:16:37 +0700

    edit csv

--- start.py
+++ start.py
@@ -2,14 +2,21 @@ from persistence_data import models
 
 models.main()
 
-file = open('movies.csv')
-csvreader = models.csv.reader(file)
+file_movie = open('./data/movie-review - movie.csv')
+csv_movie = models.csv.reader(file_movie)
 
-header = next(csvreader)
-for row in csvreader:
- movie = models.Movie(title=row[0],genre=row[1],lead_studio=row[2],profit=float(row[4]),ww_gross=row[6],year=int(row[7]))
+file_review = open('./data/movie-review - review.csv')
+csv_review = models.csv.reader(file_review)
+
+header = next(csv_movie)
+header = next(csv_review)
+
+for row in csv_movie:
+ movie = models.Movie(title=row[0],genre=row[1],lead_studio=row[2],profit=float(row[3]),ww_gross=row[4],year=int(row[5]))
  models.session.add(movie)
  models.session.commit()
- review = models.Review(movie_key=int(movie.id), audience_score=int(row[3]),rt_score=int(row[5]))
- models.session.add(review)
- models.session.commit()
+ rev = next(csv_review)
+ if row[0] == rev[0]:
+     review = models.Review(movie_key=int(movie.id), audience_score=int(rev[1]),rt_score=int(rev[2]))
+     models.session.add(review)
+     models.session.commit()
